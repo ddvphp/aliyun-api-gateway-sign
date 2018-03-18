07d6fc614e727c43d8bcc4cb50623eab063441e0
hua <yuchonghua@163.com>
2018-03-18 12:30:01 +0800

    AliyunApiGatewaySign

---
+++ README.md
@@ -0,0 +1,16 @@
+aliyun-api-gateway-sign
+===================
+
+Installation - 安装
+------------
+
+```bash
+composer require ddvphp/aliyun-api-gateway-sign
+```
+
+
+Usage - 使用
+-----
+
+
+aliyun api gateway request signature demo by php
---
+++ src/AliyunApiGatewaySign.php
@@ -0,0 +1,16 @@
+<?php
+namespace DdvPhp;
+
+use DdvPhp\AliyunApiGatewaySign\Http\Request;
+
+
+/**
+ * Class Cors
+ *
+ * Wrapper around PHPMailer
+ *
+ * @package DdvPhp\AliyunApiGatewaySign
+ */
+class AliyunApiGatewaySign extends Request
+{
+}
